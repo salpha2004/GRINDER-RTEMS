602f1f1b8c922e5d8309bbf1f8e25d02f89a0178
SAeeD <salpha.2004@gmail.com>
2015-08-03 13:00:01 +0200

    further commented QemuAbstraction

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ This repository is an extension to [GRINDER] generic fault injection framework t
 
 GRINDER should be used as a Git Submodule. This is because GRINDER gets updated and this way facilitates merging new changes to your GRINDER submodule.
 
-#### Build Instructions
-1. Get Maven and JDK (Maven 3.2.5 and JDK 7u21 has been tested and is working).
-2. Get MySQL Server.
-3. Run MySQL Server and create a user and a database:
+#### Build and Run Instructions
+**1.** Get Maven and JDK (Maven 3.2.5 and JDK 7u21 has been tested and is working).
+**2.** Get MySQL Server.
+**3.** Run MySQL Server and create a user and a database:
 ```sh
 bash$ mysql -u root
 mysql> create user 'grinder'@'localhost' identified by 'grinder';
@@ -15,28 +15,36 @@ mysql> grant all privileges on *.* to 'grinder'@'localhost';
 mysql> create database grinder;
 mysql> quit;
 ```
-4. Build grinder-rtems:
+**4.** Build grinder-rtems:
 ```sh
+bash$ cd grinder-rtems
 grinder-rtems$ mvn clean install package
 ```
-5. Apply GRINDER.patch to GRINDER:
+**5.** Apply GRINDER.patch to GRINDER:
 ```sh
 bash$ cd GRINDER
 GRINDER$ patch -p1 < ../GRINDER.patch
 ```
-6. Build and run GRINDER:
+**6.** Build and run GRINDER:
 ```sh
 GRINDER$ mvn clean install package -Dmaven.test.skip=true
 GRINDER$ java -jar client/core/target/grinder.client.core-0.0.1-SNAPSHOT.jar
 ```
 
+**7.** Introduce Target System:
+Open *grinder-rtems/src/main/resources/rtems-qemu-abstraction.xml* as GRINDER's Target System.
+
+**8.** Choose a Campaign by pressing *Load* Button.
+
+**9.** Press *Start* and you will see a Qemu instance pops out.
+
 #### Revision history
 | Version     | Date | Description   |
 | :-------: | :----: | :---: |
+| 1.2 | 03.08.15 | Further completed Build (and Run) Instructions |
 | 1.1 | 30.06.15 | Added and updated Build Instructions section |
 | 1.0 | 26.06.15 |  Initial version    |
 
 
 [GRINDER]:https://github.com/DEEDS-TUD/GRINDER
 [RTEMS]:https://www.rtems.org/
-
--- grinder-rtems/src/main/java/de/grinder/rtems/QemuAbstraction.java
+++ grinder-rtems/src/main/java/de/grinder/rtems/QemuAbstraction.java
@@ -17,10 +17,14 @@ public class QemuAbstraction implements CUEAbstraction {
 	private Process qemu;
 	
 	public QemuAbstraction () {
+		/* initialize logger. */
 		LOGGER = LoggerFactory.getLogger(String.format("%s",
 			QemuAbstraction.class.getName()));
 	}
 	
+	/* output of Qemu is passed to this function to process it and extract
+	 * each test case's result. currently it returns a fix number to test
+	 * that storing the result in DB works. */
 	private short parseTestOutput (BufferedReader testOut) throws IOException {
 		String outputStr;
 		while ( (outputStr = testOut.readLine()) != null )
@@ -28,22 +32,35 @@ public class QemuAbstraction implements CUEAbstraction {
 		return 5;
 	}
 	
+	/* "start" is supposed to initialize target system. for instance if
+	 * there is a debugger attached, start-up debugger and prepare the
+	 * environment. in this implementation, initialization is simple and
+	 * this functions runs experiment as well. */
 	@Override
 	public void start() {
+		/* build execution command which will run in Terminal. */
 		String execCmd = "qemu-system-i386 -kernel ";
 		LOGGER.info ("Starting Qemu...");
 		try {
 			/* [REMAINED] how to get the testcase path the better way?
 			 * more generic than campaign0.testcase0 */
-			/* get path to the test case image from DB. */
+			
+			/* path to the image under test is stored at DB. an interface
+			 * is provided by GrinderClient to get the path. */
+			/* get the first campaign. */
 			Campaign campaign0 = (Campaign)(GrinderClient.getGrinder().getDatabase().getCampaigns().toArray()[0]);
 			if (campaign0 != null) {
+				/* get the first test case (in the first campaign). */
 				ExperimentRun experiment = new ExperimentRun (campaign0.getTestCases().get(0), campaign0);
+				/* get image under test's path from the test case.
+				 * this value has been initialized manually via
+				 * mysql's command line interface. */
 				execCmd = execCmd + campaign0.getTestCases().get(0).getModule() + " -serial /dev/stdout";
 				qemu = Runtime.getRuntime().exec(execCmd);
 				BufferedReader qemuOutput = new BufferedReader(
 					new InputStreamReader(qemu.getInputStream()));
 				experiment.setResult (parseTestOutput (qemuOutput));
+				/* store back test's result in DB. */
 				experiment.save ();
 			}
 		}
@@ -52,16 +69,20 @@ public class QemuAbstraction implements CUEAbstraction {
 		}
 	}
 
+	/* stop target system. in this case, simply kill qemu process. */
 	@Override
 	public void stop() {
-		
+		LOGGER.info ("Stopping Qemu...");
+		qemu.destroy ();
 	}
 
+	/* "runExperiment" is called after "start" to actually run the experiment.
+	 * in this implementation "start" takes care of running the experiment
+	 * as well. */
 	@Override
-	public void runExperiment() {
-		
-	}
+	public void runExperiment() {}
 
+	/* reset, that is, "stop", "start", and "runExperiment" again. */
 	@Override
 	public void reset() {
 		
